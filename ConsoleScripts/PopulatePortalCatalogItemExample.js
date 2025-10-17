61a1f9e0ec69da22c1f8b88624e03197df2802c5
OosukeRen <oosuke_ren@abv.bg>
2025-10-17 18:48:00 +0300

    adding catalog item utilites for portals for easy population when testing or developing.

---
+++ ConsoleScripts/PopulatePortalCatalogItemExample.js
@@ -0,0 +1,138 @@
+/**
+ * Create an async wait
+ * e.g await wait(1000);
+ * @param {*} milliseconds 
+ * @returns {Promise<void>}
+ */
+async function wait (milliseconds) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, milliseconds);
+    })
+};
+
+/**
+ * 
+ * @returns {{
+ *  setValue: (fieldName: string, value: any) => void;
+ *  getValue: (fieldName: string) => any;
+ *  submit: () => any;
+ * }}
+ */
+function getGForm () {
+    return angular.element($("sp-variable-layout")).scope().getGlideForm();
+};
+
+/**
+ * 
+ * @param {*} formToPopulate 
+ * @param {{delay: number, shouldSubmit: boolean}} config
+ * - delay: time inbetween separate field populations (in case you have some client scripts that are influenced by specific fields/values;
+ * - shouldSubmit: mark whether the form should get submitted after populating everything - would work for both catalog item and variable set modals.  
+ */
+async function populate (formToPopulate, config) {
+    let {delay = 1, shouldSubmit} = config;
+    let g_form = getGForm(); // if we've opened a modal, the old g_form is going to hold a reference to the main catalog item, not modal's fields, so we need a new reference for every time we're populating;
+    let entries = Object.entries(formToPopulate);
+
+    for(let [key, value] of entries) {
+        g_form.setValue(key, value);
+        await wait(delay);
+    }
+
+    if(shouldSubmit) {
+        g_form.submit();
+    }
+};
+
+function isVisible(el) {
+  if (!el) return false;
+  const style = getComputedStyle(el);
+  return (
+    style.display !== 'none' &&
+    style.visibility !== 'hidden' &&
+    style.opacity !== '0' &&
+    el.offsetWidth > 0 &&
+    el.offsetHeight > 0
+  );
+}
+
+/**
+ * run this inside a portal so you can make a snapshot of all of the fields you have on your catalog item, and it'll be logged in the console so you can turn it into a template
+ */
+function exportForm () {
+    let g_form = getGForm();
+    let visibleItems = Array.from($(`[ng-repeat='f in ::column.fields']`)).filter(isVisible);
+    let fieldKeys = visibleItems.map(el => el.id);
+    let formToPrint = {};
+
+    for(key of fieldKeys) {
+        let value = g_form.getValue(key);
+        formToPrint[key] = value;
+    }
+
+    console.log(formToPrint);
+}
+
+/**
+ * @description Searches for the first variable set with the name passed, searches for the first button with specific label, and then triggers a click event on it
+ * @param {*} variableName 
+ * @param {*} buttonText 
+ */
+function clickMultiRowVariableButton (variableName, buttonText) {
+    Array.from($(`sp-sc-multi-row-element[id*=${variableName}] button`)).filter(button => {
+        return button.textContent.includes(buttonText)
+    })[0].click;
+}
+
+function submit () {
+    let g_form = getGForm();
+    g_form.submit();
+}
+
+
+// EXAMPLE STARTS FROM HERE =======================================Prepare yourself a file where you paste everything until this line ===============================================
+
+const exampleForm1 = {
+    field1: "Nick Fury",
+    field12: true
+};
+
+const exampleForm2 = {
+    field3: 20,
+    field4: 25
+};
+
+const exampleMultiRowData = [
+    {
+        field1: 15,
+        field2: "blue"
+    },
+    {
+        field1: 20,
+        field2: "yellow"
+    }
+];
+
+const exampleMultiRowVariableSetName = "example_multi_row_variable_set_name"
+
+(async() => {
+    await populate(exampleForm1, {delay: 20});
+    await wait(100);
+    await populate(exampleForm2, {delay: 20});
+    
+    for(let record in exampleMultiRowData) {
+        clickMultiRowVariableButton("example_multi_row_variable_set_name", "Add")                     // searches for the "field" on the portal, then clicks the button with Text "Add";
+        await wait(1000);                                                                           // We wait for the variable set modal to show up and load;
+        await populate(record, {delay: 20, shouldSubmit: true});                                    // we populate the necessary fields
+    }
+
+    // OR you could just do this;
+
+    await populate({
+        example_multi_row_variable_set_name: '[{field: 15, field2: "blue"}, {field1: 20, field2: "yellow"}]'
+    });
+
+    submit();
+})()
